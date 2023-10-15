11a71257168aa478289b4ffb599b2ad65fa72252
Maximilian Schreiter <m.schreiter@micromata.de>
2023-10-15 09:29:23 +0200

    Add setting struct

--- src/model/setting.rs
+++ src/model/setting.rs
@@ -1,1 +1,29 @@
-// TODO
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Setting {
+    id: String,
+    value: String,
+}
+
+impl Setting {
+    pub fn new() -> Self {
+        Setting {
+            id: String::new(),
+            value: String::new(),
+        }
+    }
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+    pub fn value(&self) -> &str {
+        &self.value
+    }
+    pub fn set_id(&mut self, id: String) {
+        self.id = id;
+    }
+    pub fn set_value(&mut self, value: String) {
+        self.value = value;
+    }
+}
